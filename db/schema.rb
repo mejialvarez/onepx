b5b3e88e2dca5daa59df749ec63bbb6a85aa647b
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2015-08-30 22:08:41 -0500

    Setup carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150830174837) do
+ActiveRecord::Schema.define(version: 20150831025248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150830174837) do
     t.string   "tags",        default: [],              array: true
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "photo"
   end
 
 end

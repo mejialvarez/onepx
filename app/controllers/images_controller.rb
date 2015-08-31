0046b26c2db5913300865c34e8af85e0b9dbbd41
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2015-08-30 21:30:29 -0500

    Add images controller with index, new and create actions

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,25 @@
+class ImagesController < ApplicationController
+
+  def index
+    @images = Image.all
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      redirect_to images_path, notice: t('.created', model: @image.class.model_name.human)
+    else
+      render :new
+    end
+  end
+
+  private
+    def image_params
+      params.require(:image).permit(:name, :description, :category, :tags_text)
+    end
+end
